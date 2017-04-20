a45d239c7d00b59e51c787e1dced68baa5032045
Jairo <jairo@indigitall.net>
2017-04-20 12:19:21 +0200

    init

---
+++ src/Views/Index/Index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Layout from 'Layout/Layout';
+
+class Index extends Component {
+  render() {
+    return (
+      <Layout>
+        index
+      </Layout>
+    );
+  }
+}
+
+export default Index;
